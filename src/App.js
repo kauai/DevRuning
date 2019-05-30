193d89487c7833dd776e616051ff8185b914eb9a
Thiago kauai <html5ephp@gmail.com>
2019-05-30 10:55:25 -0300

    save commit trabalhando com jwt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React,{ Component } from 'react';
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component{
+
+  async componentDidMount(){
+    let Htoken = localStorage.getItem('@token-runing')
+
+    if(!Htoken){
+      const { data:{ token }} = await axios.post('http://localhost:3001/users/login',{
+        email:"tuliofaria@devpleno.com",
+        passwd:"abc123"
+      })
+      Htoken = token
+      localStorage.setItem('@token-runing',token)
+      console.log('Passou no Login')
+    }
+      const decoded = jwtDecode(Htoken)
+      console.log(decoded)
+
+  }  
+
+  render(){
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
